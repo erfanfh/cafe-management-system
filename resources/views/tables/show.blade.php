3d7314cebccfd6730a23e66c36a833c967be1f8c
erfanfh <erfanfarokhi84@gmail.com>
2024-08-27 11:48:06 +0330

    authorizing orders

--- resources/views/tables/show.blade.php
+++ resources/views/tables/show.blade.php
@@ -2,20 +2,26 @@
 @section('title', $table->name . " ($key) " )
 @section('content')
     <div class="display-6"> میز {{$table->name . "(" . $key .")"}}</div>
-    <div class="d-flex">
-        <div class="h3">افزودن سفارش</div>
-        <form id="addProduct" action="{{ route('orders.store', $table) }}" method="post">
-            @csrf
-            <div class="mb-3 d-flex gap-3">
-                <select class="form-select" onchange="document.getElementById('addProduct').submit()" name="name">
-                    <option selected disabled>سفارش</option>
-                    @foreach($products as $order)
-                        <option value="{{$order->id}}">{{ $order->name . " - " . number_format($order->price) . " تومان "}}</option>
-                    @endforeach
-                </select>
-            </div>
-        </form>
-    </div>
+    @if(!$table->status)
+        <div class="d-flex">
+            <div class="h3">افزودن سفارش</div>
+            <form id="addProduct" action="{{ route('orders.store', $table) }}" method="post">
+                @csrf
+                <div class="mb-3 d-flex gap-3">
+                    <select class="form-select" onchange="document.getElementById('addProduct').submit()" name="name">
+                        <option selected disabled>سفارش</option>
+                        @foreach($products as $order)
+                            @if($order->status)
+                                <option
+                                    value="{{$order->id}}">{{ $order->name . " - " . number_format($order->price) . " تومان "}}
+                                </option>
+                            @endif
+                        @endforeach
+                    </select>
+                </div>
+            </form>
+        </div>
+    @endif
     @if(session('success'))
         <div class="alert alert-success">{{ session('success') }}</div>
     @endif
@@ -29,6 +35,7 @@
                 <th scope="col">قیمت</th>
                 <th scope="col">تعداد</th>
                 <th scope="col">عملیات</th>
+                <th scope="col">توضیحات</th>
             </tr>
             </thead>
             <tbody>
@@ -43,11 +50,13 @@
                     <td>{{ number_format($order->product->price) }}</td>
                     <td>{{ $order->quantity }}</td>
                     <td class="d-flex gap-2">
-                        <form action="{{ route('orders.update', $order->id) }}" method="post">
-                            @csrf
-                            @method('PUT')
-                            <button class="btn btn-outline-success" type="submit">افزودن</button>
-                        </form>
+                        @if($order->product->status)
+                            <form action="{{ route('orders.update', $order->id) }}" method="post">
+                                @csrf
+                                @method('PUT')
+                                <button class="btn btn-outline-success" type="submit">افزودن</button>
+                            </form>
+                        @endif
 
                         <form action="{{ route('orders.destroy', $order->id) }}" method="post">
                             @csrf
@@ -55,24 +64,32 @@
                             <button class="btn btn-outline-danger" type="submit">کاستن</button>
                         </form>
                     </td>
+                    <td>
+                        @if(!$order->product->status)
+                            <div class="text-danger">امکان سفارش بیشتر از این محصول وجود ندارد</div>
+                        @endif
+                    </td>
                 </tr>
             @endforeach
             </tbody>
         </table>
     </div>
-    <div>
-        <div class="h-100 p-5 border border-dark rounded-3 d-flex flex-column justify-content-center align-items-end gap-3">
-            <h1>صورتحساب</h1>
-            <div>
-                تعداد سفارشات :
-                <span class="fw-bold fs-large">{{$totalQuantity}}</span>
+    @if(!$table->status)
+        <div>
+            <div
+                class="h-100 p-5 border border-dark rounded-3 d-flex flex-column justify-content-center align-items-end gap-3">
+                <h1>صورتحساب</h1>
+                <div>
+                    تعداد سفارشات :
+                    <span class="fw-bold fs-large">{{$totalQuantity}}</span>
+                </div>
+                <div>
+                    قیمت کل :
+                    <span class="fw-bold fs-large">{{ number_format($totalPrice) }}</span>
+                    تومان
+                </div>
+                <button class="btn btn-primary">پرداخت</button>
             </div>
-            <div>
-                قیمت کل :
-                <span class="fw-bold fs-large">{{ number_format($totalPrice) }}</span>
-                تومان
-            </div>
-            <button class="btn btn-primary">پرداخت</button>
         </div>
-    </div>
+    @endif
 @endsection
